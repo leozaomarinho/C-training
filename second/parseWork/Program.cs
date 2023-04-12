d68dbec5dcc60ed47125f2899526bc37625f2457
Leonardo Marinho <leowalkg6@Gmail.com>
2023-04-12 17:20:09 -0300

    add new files, working at casting

---
+++ second/parseWork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UsandoParse
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            double a = 5.8;
+            float b;
+
+            //a = b;
+
+            b = (float)a;
+            Console.WriteLine(b);
+        }
+    }
+}
