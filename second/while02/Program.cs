c5e33c2db600d9be6b4a6b8a6b7d6078728f74b9
Leonardo Marinho <leowalkg6@Gmail.com>
2023-04-14 16:15:17 -0300

    add while exe

---
+++ second/while02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace While
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int pass = 2002;
+
+            Console.WriteLine("Digite sua senha:");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != pass)
+            {
+
+                Console.WriteLine("Senha invalida!");
+                Console.WriteLine("Digite sua senha!");
+                senha = int.Parse(Console.ReadLine());
+
+
+            }
+
+            Console.WriteLine("Acesso permitido!");
+
+
+
+        }
+    }
+
+}
