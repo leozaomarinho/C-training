3855a8b61a10bfa57524444d89903be6d2d252a8
Leonardo Marinho <leowalkg6@Gmail.com>
2023-04-16 22:10:26 -0300

    working with class and methods

---
+++ classes/ExeFuncionario/FuncionarioDados.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeFuncionario
+{
+    public class FuncionarioDados
+    {
+
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() { 
+        
+            return SalarioBruto - Imposto;
+
+        }
+        public void AumentarSalario(double porcentagem) { 
+        
+            double ajuste = (SalarioBruto * porcentagem)/100;
+
+        SalarioBruto = SalarioBruto+ajuste;
+        
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $" + SalarioLiquido();
+        }
+
+
+    }
+}
---
+++ classes/ExeFuncionario/Program.cs
@@ -0,0 +1,35 @@
+﻿using ExeFuncionario;
+using System.Globalization;
+
+
+namespace DadosFuncionario
+{
+    class Program
+    {
+
+
+        static void Main(string[] args) { 
+        
+            FuncionarioDados func = new FuncionarioDados();
+
+            Console.WriteLine("Dados do funcionario:");
+            Console.Write("Nome:");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto");
+            func.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Imposto:");
+            func.Imposto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Funcionario:{func}");
+
+            Console.Write("Digite a porcentagem para aumentar o salario:");
+            double aumento = double.Parse(Console.ReadLine());
+
+            func.AumentarSalario(aumento);
+
+            Console.WriteLine($"dados Atualizados:{func}");
+
+
+        }
+    }
+}
