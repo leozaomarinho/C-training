90250ce285bf58e49c9527e281ded111e57ddf69
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-24 19:08:35 -0300

    add calculo terreno

---
+++ Revisao/precoTerreno/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Program { 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, area, precoMetroQuadrado;
+
+            Console.WriteLine("Digite a largura do terreno:");
+            largura= double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o comprimento do terreno:");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o valor do metro quadrado:");
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+
+            double valorTerreno = area * precoMetroQuadrado;
+
+            Console.WriteLine("Total da area do terreno: " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor total do terreno: " + valorTerreno.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+
+
+}
