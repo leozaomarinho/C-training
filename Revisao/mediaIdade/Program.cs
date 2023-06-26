daca988fb661b10d8769fab763a1f19d028a1eb5
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-26 17:57:16 -0300

    add media idade

---
+++ Revisao/mediaIdade/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace calculaIdade
+{
+    class Programa {
+
+        static void Main(string[] args)
+        {
+            double p1, p2, media;
+
+            Console.WriteLine("Digite a idade da primeira pessoa:");
+            p1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite a idade da segunda pessoa:");
+            p2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (p1 + p2) / 2;
+
+            Console.WriteLine("A idade media de maria e joaquim é de: "+media+" anos");
+
+
+
+        }
+    
+    }
+
+}
