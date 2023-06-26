0e92f382dd59c76097342bd29c73dcb712a2f183
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-26 18:33:43 -0300

    add novos recursos

--- Revisao/mediaIdade/Program.cs
+++ Revisao/mediaIdade/Program.cs
@@ -8,16 +8,22 @@ namespace calculaIdade
 
         static void Main(string[] args)
         {
+            string nome1,nome2;
             double p1, p2, media;
 
+            Console.WriteLine("Digite o nome da primeira pessoa:");
+            nome1 = Console.ReadLine();
             Console.WriteLine("Digite a idade da primeira pessoa:");
             p1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o nome da segunda pessoa:");
+            nome2 = Console.ReadLine();
             Console.WriteLine("Digite a idade da segunda pessoa:");
             p2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             media = (p1 + p2) / 2;
 
-            Console.WriteLine("A idade media de maria e joaquim é de: "+media+" anos");
+            Console.WriteLine("A idade media de "+nome1+ " e "+nome2+" é de: "+ media.ToString("F1",CultureInfo.InvariantCulture) + " anos");
 
 
 
