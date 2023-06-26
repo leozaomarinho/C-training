6a2a1a64ed64814ccf57c84a2bb372f13704971e
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-26 17:38:42 -0300

    add exercicio com Math.sqrt e Math.Pow

---
+++ Revisao/atividade02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            double largura, altura, area, perimetro, diagonal;
+
+            Console.WriteLine("Digite o valor da base:");
+            largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o valor da altera:");
+            altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * altura;
+
+            perimetro = (altura * 2) + (largura * 2);
+
+            diagonal = Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
+
+            Console.WriteLine("Area =" +area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro =" + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal =" +diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
