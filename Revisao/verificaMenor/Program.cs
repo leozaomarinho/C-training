8239c9aeab3b512789b8c5d79a9ee57022cb428f
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-27 17:04:36 -0300

    verifica menor numero

---
+++ Revisao/verificaMenor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace verifica { 
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int num1,num2,num3;
+         
+
+            Console.WriteLine("Digite o primeiro numero:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro numero:");
+            num3 = int.Parse(Console.ReadLine());
+
+
+                if (num1 <= num2 && num1 <= num3)
+                {
+                    Console.WriteLine("Menor ="+num1);
+                  
+                }
+                else if (num2 <= num1 && num2 <= num3)
+                {
+                    Console.WriteLine("Menor ="+num2);
+                   
+                }
+                else {
+                    Console.WriteLine("Menor = "+num3);
+                }
+
+              
+            }
+
+
+
+        }
+    }
+
