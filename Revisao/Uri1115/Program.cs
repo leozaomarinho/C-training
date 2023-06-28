3952c885ecae2df2049268de58db9fc4f0f78942
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-28 20:30:47 -0300

    trabalhando com coordenadas

---
+++ Revisao/Uri1115/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        string[] vet = Console.ReadLine().Split(' ');
+        int x, y;
+
+        x = int.Parse(vet[0]);
+        y = int.Parse(vet[1]);
+
+        while (x != 0 && y != 0)
+        {
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("O ponto está no primeiro quadrante.");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("O ponto está no segundo quadrante.");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("O ponto está no terceiro quadrante.");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("O ponto está no quarto quadrante.");
+            }
+            else
+            {
+                Console.WriteLine("O ponto está no quadrante nulo.");
+            }
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Um dos pontos esta no quadrante Nulo!");
+
+
+    }
+}
