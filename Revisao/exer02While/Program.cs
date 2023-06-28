1679a826b53fee6f4dfc1e8eb770a1fdc12f204b
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-28 19:10:39 -0300

    trabalhando com while e condicionais

---
+++ Revisao/exer02While/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+class Exercicio {
+
+    static void Main(string[] args)
+    {
+        double idade,media, soma = 0;
+        int cont = 0;
+
+        Console.WriteLine("Digite a idade:");
+        idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        while (idade >= 0)
+        {
+            
+                soma+= idade;
+                cont++;
+                idade = double.Parse(Console.ReadLine());
+                
+            
+           
+        }
+        if (cont == 0)
+        {
+            Console.WriteLine("Impossivel calcular!");
+        }
+        else {
+            media = soma / cont;
+
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        
+        Console.ReadLine();
+    }
+
+}
