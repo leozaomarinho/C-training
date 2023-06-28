3b0afb8cb8d45d6e59046607e402022b957cae73
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-28 18:43:13 -0300

    exercicio URI 1113

---
+++ Revisao/uri1113/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        int x,y;
+        string[] vet = Console.ReadLine().Split(' ');
+
+        x = int.Parse(vet[0]);
+        y = int.Parse(vet[1]);
+
+        while (x != y) {
+
+
+            if (x > y) {
+                Console.WriteLine("Decrescente");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("Crescente");
+            }
+
+             vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+
+        }
+
+    }
+
+}
