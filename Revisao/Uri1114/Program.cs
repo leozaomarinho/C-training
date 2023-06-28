98519147c5e081e4e4589917a50511d844a2a2f0
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-28 20:14:22 -0300

    exercicio uri 1114

---
+++ Revisao/Uri1114/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args)
+    {
+        int senha;
+
+        senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha invalida");
+                senha = int.Parse(Console.ReadLine());
+            };
+        
+        Console.WriteLine("Acesso permitido");
+    }
+    
+}
