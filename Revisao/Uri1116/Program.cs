c2cd2922a423958c7f531b2d7f545d3186ccd76b
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-29 14:51:27 -0300

    uri 1116 completo

---
+++ Revisao/Uri1116/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        
+        double resultado,x,y;
+        string[] n = Console.ReadLine().Split(' ');
+
+        x = double.Parse(n[0]);
+        y = double.Parse(n[1]);
+
+        while (x>=0 && y!=0)
+        {
+            resultado = (x / y);
+
+            Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+
+            n = Console.ReadLine().Split(' ');
+
+            x = double.Parse(n[0]);
+            y = double.Parse(n[1]);
+
+        };
+
+        Console.WriteLine("divisão impossivel");
+        Console.ReadLine();
+
+
+
+
+    }
+
+}
