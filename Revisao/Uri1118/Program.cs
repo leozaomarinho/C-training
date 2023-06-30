20ea7d60f2174fc7d394238a71f69056842d6f68
Leonardo Marinho <leowalkg6@Gmail.com>
2023-06-30 10:58:27 -0300

    trabalhando no uri 1118

---
+++ Revisao/Uri1118/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args)
+    {
+        string[] vet = Console.ReadLine().Split(' ');
+        double n1, n2, media = 0;
+        int x = 0;
+
+        n1 = double.Parse(vet[0]);
+        n2 = double.Parse(vet[1]);
+
+
+        while (x!=2) {
+
+
+
+            Console.WriteLine("novo calculo: (1 - sim 2 - não )");
+            x = int.Parse(Console.ReadLine());
+        }
+
+
+
+    }
+
+}
