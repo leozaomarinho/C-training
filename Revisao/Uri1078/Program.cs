981127fae3233805ba644ab5432cb2fe1ab35afb
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-03 10:58:55 -0300

    adicionando uri 1078

---
+++ Revisao/Uri1078/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int entrada, resultado;
+
+        Console.WriteLine("Digite o valor que você deseja ver a taboada:");
+        entrada = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i<=10;i++)
+        {
+            resultado = i * entrada;
+            Console.WriteLine($"{i} x {entrada} = {resultado}");
+
+        }
+
+        Console.WriteLine("Taboada encerrada.");
+    }
+}
