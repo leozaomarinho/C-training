bb0f4c3aad5ebf0205bb920fd8589546fbf84f07
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-05 17:09:14 -0300

    uri 1067 resolvido

---
+++ Revisao/Uri1067/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Uri {
+    static void Main(string[] args)
+    {
+        int x;
+        
+        int i = 0;
+
+        x = int.Parse(Console.ReadLine());
+
+
+        do
+        {
+            int impar = 0;
+
+            if (i % 2 != 0)
+            {
+                impar = i;
+
+                Console.WriteLine(impar);
+
+            }
+      
+            i++;
+
+        }
+        while (i < x);
+    }
+}
