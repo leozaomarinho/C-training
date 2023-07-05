2a41a6e38dade759e67fcc4fecd91d06b0e98231
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-05 17:53:45 -0300

    Uri 1073 concluido

---
+++ Revisao/Uri1073/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Uri
+{
+    static void Main(string[] args)
+    {
+        int N,i=1, quadrado;
+        
+
+        Console.WriteLine("Digite o valor de N:");
+        N = int.Parse(Console.ReadLine());
+
+        do {
+
+            if (i % 2 == 0)
+            {
+                quadrado =(int) Math.Pow(i,2);
+
+                Console.WriteLine($"{i} ^ 2 = {quadrado}");
+            }
+
+            i++;
+        } while (i<=N);
+    }
+}
