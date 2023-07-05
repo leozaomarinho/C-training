d2fec2e7396ee77a7016ac34c626b968927a783b
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-04 22:38:19 -0300

    URI 1071 resolvido

---
+++ Revisao/URI1171/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Channels;
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        int x, y, menor, maior, soma=0;
+
+        Console.WriteLine("Digite o primeiro valor:");
+        x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo valor:");
+        y = int.Parse(Console.ReadLine());
+
+        if (x < y)
+        {
+            menor = x;
+            maior = y;
+
+        }
+        else
+        {
+            menor = y;
+            maior = x;
+        }
+
+        for(int i = menor + 1; i < maior; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma += i;
+            }
+
+            else
+            {
+                continue;
+            }
+        };
+
+        Console.WriteLine($"A soma dos números impares do intervalo é: {soma}");
+    }
+
+}
