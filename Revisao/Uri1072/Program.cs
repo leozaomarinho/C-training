e615f3bbb8a17e15295cba113daf487a21102b1f
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-05 17:34:41 -0300

    Uri 1072 resolvido

---
+++ Revisao/Uri1072/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Uri
+{
+    static void Main(string[] args)
+    {
+        int n,x, fora, dentro;
+        Console.WriteLine("Digite o valor de um número inteiro:");
+        n = int.Parse(Console.ReadLine());
+        x = 0;
+        fora = 0;
+        dentro = 0;
+        do {
+
+            if (x >= 10 && x <= 20)
+            {
+                dentro += 1;
+            }
+            else
+            {
+                fora += 1;
+            }
+
+            x++;
+        }while (x<n);
+
+        Console.WriteLine($"{dentro} dentro.");
+        Console.WriteLine($"{fora} fora.");
+    }
+}
