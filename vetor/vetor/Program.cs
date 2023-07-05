047bbecd5e02c99ed8ebbd2317fe25c31366b6ec
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-05 20:41:10 -0300

    adicionei novo projeto errado

---
+++ vetor/vetor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Progra
+{
+    static void Main(string[] args)
+    {
+        int N;
+        string[] vet;
+       
+
+        N = int.Parse(Console.ReadLine());
+
+        vet = new string[N];
+
+        vet=Console.ReadLine().Split(' ');
+
+        for(int i = 0; i < N; i++)
+        {
+            int num = 0;
+
+            num = int.Parse(vet[i]);
+
+            if (num<0)
+            {
+                Console.WriteLine($"{num}");
+            }
+
+        }
+    }
+}
