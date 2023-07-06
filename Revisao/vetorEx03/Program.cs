266c1305346f5aef4288acfd1eb065fe0173083a
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-06 17:28:43 -0300

    atividade de vetor 03 em construção.

---
+++ Revisao/vetorEx03/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+class Program {
+    static void Main(string[] args)
+    {
+        int N,i=0;
+
+        string[] nome;
+        int[] idade;
+        double[] altura;
+        double media = 0,menor=0;
+
+        
+
+        Console.WriteLine("Digite o valor de N:");
+        N = int.Parse(Console.ReadLine());
+
+        nome = new string[N];
+        idade = new int[N];
+        altura = new double[N];
+
+
+
+        do
+        {
+            Console.WriteLine("Digite o nome(apenas o primeiro nome), idade e altura da pessoa:");
+            string[] s = Console.ReadLine().Split(' ');
+
+            nome[i] = s[i];
+            idade[i] = int.Parse(s[i+1],CultureInfo.InvariantCulture);
+
+            if (idade[i] < 16)
+            {
+                menor++;
+            }
+            altura[i] = double.Parse(s[i+2],CultureInfo.InvariantCulture);
+
+            media+=(double)idade[i];
+
+            i++;
+
+        } while (i < N);
+
+        media = media / N;
+
+
+    }
+}
