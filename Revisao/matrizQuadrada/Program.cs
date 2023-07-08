0251f8b89e9c03e00b5ea28b2def397693506dd1
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-08 18:47:10 -0300

    exercicios com matriz exibindo diagonal

---
+++ Revisao/matrizQuadrada/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N ,negativo=0;
+        int[,] mat;
+
+       
+
+        Console.WriteLine("Digite o valor da matriz quadrada:");
+        N = int.Parse(Console.ReadLine());
+
+
+        mat = new int[N, N];
+
+        for (int i =0;i<N;i++)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+           
+            for (int j = 0; j < N; j++)
+            {
+                
+                mat[i, j] = int.Parse(s[j]);
+
+                if (mat[i, j] < 0)
+                {
+                    negativo++;
+                }
+
+            }
+            
+        }
+
+
+
+        Console.WriteLine($"Diagonal principal: ");
+            for(int i =0; i < N; i++)
+        {
+            Console.Write(mat[i, i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Quantidade de negativos: {negativo}");
+
+    }
+}
