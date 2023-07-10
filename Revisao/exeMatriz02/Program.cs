c0e16304b42bcfbd5a174e1f825515f48b9353a8
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-10 14:54:19 -0300

    fazendo debug e concertando

---
+++ Revisao/exeMatriz02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int M, N;
+
+        int [,] matriz;
+        int soma = 0;
+
+        Console.WriteLine("Digite a quantidade de linhas: ");
+        string[] sup = Console.ReadLine().Split(' ');
+
+        M = int.Parse(sup[0]);
+        N = int.Parse(sup[1]);
+
+        matriz = new int[M, N];
+
+        for(int i =0; i <M; i++)
+        {
+            for(int j = 0; j < N; j++)
+            {
+                Console.WriteLine("Digite os numeros inteiros na mesma linha:");
+                string[] vet = Console.ReadLine().Split(' ');
+
+                matriz[i,j]= int.Parse(vet[j]);
+
+                soma += matriz[i, j];
+
+
+            }
+        }
+
+        Console.WriteLine(soma);
+
+
+    }
+}
