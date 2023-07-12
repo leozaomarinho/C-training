47e9344fcc9b7a79ee486b9a841be5b7e7206be3
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-12 12:31:20 -0300

    trabalhando com classes e metodos

---
+++ Revisao/03poo/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03poo
+{
+    internal class Produto
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+           double total = Quantidade* Preco;
+
+            return total;
+
+        }
+
+        public void AdicionarProdutos(int quant)
+        {
+            Quantidade += quant;
+
+            Console.WriteLine($"Você adicionou {quant} unidades de {Nome} ao estoque.");
+        }
+
+        public void RemoverProdutos(int quant)
+        {
+            Quantidade -= quant;
+
+            Console.WriteLine($"Você adicionou {quant} unidades de {Nome} ao estoque. ");
+        }
+
+    }
+}
---
+++ Revisao/03poo/Program.cs
@@ -0,0 +1,40 @@
+﻿using _03poo;
+using System;
+
+namespace program {
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto TV;
+
+            TV = new Produto();
+
+            Console.WriteLine("Adicionando produtos ao estoque:");
+
+            Console.WriteLine("Digite o nome do produto:");
+            TV.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o preço do produto:");
+            TV.Preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade do produto:");
+            TV.Quantidade = int.Parse(Console.ReadLine());
+
+
+            int options =0;
+
+            do {
+                Console.WriteLine("O que você deseja fazer:");
+                Console.WriteLine(" 0 - Ver valor total em estoque");
+                Console.WriteLine(" 1 - Adicionar produtos ao estoque");
+                Console.WriteLine(" 2 - Remover produtos do estoque");
+                Console.WriteLine(" 3 - Sair");
+
+                options = int.Parse(Console.ReadLine());
+
+            } while (options!=3);
+        }
+    }
+}
