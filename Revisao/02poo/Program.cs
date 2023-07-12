e81436e403f906f3b37e022fedb6ba9e1d9d41c2
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-12 12:11:00 -0300

    testando funcoes

---
+++ Revisao/02poo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Digite o valor de A:");
+                a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B:");
+            b = int.Parse(Console.ReadLine());
+
+            soma(a, b);
+
+        }
+
+
+        static void soma(int a,int b)
+        {
+            int resultado = a + b;
+
+            Console.WriteLine($"{resultado}");
+
+        }
+    }
+}
