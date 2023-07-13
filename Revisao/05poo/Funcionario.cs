51a6463ec1afeae7625615d367d64643a7c5939d
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-13 14:49:08 -0300

    construindo classe Funcionario

---
+++ Revisao/05poo/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05poo
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+
+           double salarioLiquido = SalarioBruto - Imposto;
+            return salarioLiquido;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            double aumento = SalarioBruto * porcentagem;
+
+            SalarioBruto += aumento;
+        }
+
+        public override string ToString()
+        {
+            return $"Dados atualizados: {Nome}, $ {SalarioLiquido()}";
+        }
+    }
+}
