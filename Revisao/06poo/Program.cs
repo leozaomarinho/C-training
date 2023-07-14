07250d1806c7b969907e287bcad97a9e228d9498
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-14 17:34:52 -0300

    exercicio 06 finalizado

--- Revisao/06poo/Program.cs
+++ Revisao/06poo/Program.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using _06poo;
+using System;
+using System.Globalization;
 
-namespace program{
+namespace program
+{
 
     class Program
     {
         static void Main(string[] args)
         {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno:");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a primeira nota: ");
+            aluno.N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite a segunda nota:");
+            aluno.N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite a terceira nota:");
+            aluno.N3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("------------------------------------------------------------");
+            aluno.Aprovacao();
+
+
 
         }
     }
