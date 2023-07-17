759ed24f5a779ee54f3b19fa4e7437d57768fd6e
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-17 17:39:11 -0300

    adicionando metodo de cadastro e de listagem de produtos!

--- Revisao/Estoque/Produto.cs
+++ Revisao/Estoque/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,15 +21,23 @@ namespace Estoque
         public void CadastrarProduto(string nome,double valor,int quantidade)
         {
             Produtos[TotalProdutos, 0] = nome;
-            Produtos[TotalProdutos, 1] = valor.ToString();
+            Produtos[TotalProdutos, 1] = valor.ToString(CultureInfo.InvariantCulture);
             Produtos[TotalProdutos,2] = quantidade.ToString();
             TotalProdutos++;
  
         }
 
+        public void ListarProdutos()
+        {
+            for (int i = 0; i < TotalProdutos; i++)
+            {
+                Console.WriteLine($"Produto: {Produtos[i,0]}, Valor do produto: {Produtos[i,1]}, Quantidade do produto: {Produtos[i,2]}");
+            }
+        }
+
         public override string ToString()
         {
-            return $"";
+            return $"Nome : {Produtos[TotalProdutos,0]}, Valor do produto: {Produtos[TotalProdutos,1]}, Quantidade do produto: {Produtos[TotalProdutos,2]}";
         }
 
     }
--- Revisao/Estoque/Program.cs
+++ Revisao/Estoque/Program.cs
@@ -13,8 +13,24 @@ namespace program
             Console.WriteLine("2 - Remover produtos.");
             Console.WriteLine("3 - Ver Estoque.");
             Console.WriteLine("0 - Sair.");
+            opcao = int.Parse(Console.ReadLine());
 
-            switch(opcao)
+            switch (opcao)
+            {
+                case 0:
+                    Console.WriteLine("Você escolheu sair!");
+                    break;
+                case 1:
+                    Console.WriteLine("Você escolheu cadastrar um produto!");
+                    Console.Write("Nome do produto:");
+                        string nome = Console.ReadLine();
+                    Console.Write("Valor do produto:");
+                        double valor = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Quantidade do produto:");
+                        int quant = int.Parse(Console.ReadLine());
+                    break;
+                case 2:
+            }
         }
     }
 }
