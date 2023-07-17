5edb72f5b7b3dd46a78c1f874d08b54b6e6e5739
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-17 17:57:41 -0300

    classe lista produtos e adicionar produtos já funcionando!

--- Revisao/Estoque/Produto.cs
+++ Revisao/Estoque/Produto.cs
@@ -14,24 +14,27 @@ namespace Estoque
 
        public Produto()
         {
+            //Limitados o cadastro dos produtos a 500, para não haver a necessidade de criar uma logica para definir o tamanho da matriz
             Produtos = new string[500,3];
             TotalProdutos = 0;
         }
 
+        //Utilizei uma matriz  na função de cadastro, onde a vertical é o total de produtos e a horizontal os campos dos produtos;
         public void CadastrarProduto(string nome,double valor,int quantidade)
         {
             Produtos[TotalProdutos, 0] = nome;
-            Produtos[TotalProdutos, 1] = valor.ToString(CultureInfo.InvariantCulture);
+            Produtos[TotalProdutos, 1] = valor.ToString("F2",CultureInfo.InvariantCulture);
             Produtos[TotalProdutos,2] = quantidade.ToString();
             TotalProdutos++;
  
         }
 
         public void ListarProdutos()
+            //utilizamos um loop for para imprimir os itens da matriz utilizando o Total de produtos cadastrados como index
         {
             for (int i = 0; i < TotalProdutos; i++)
             {
-                Console.WriteLine($"Produto: {Produtos[i,0]}, Valor do produto: {Produtos[i,1]}, Quantidade do produto: {Produtos[i,2]}");
+                Console.WriteLine($"| Produto: {Produtos[i,0]}, Valor do produto: {Produtos[i,1]}, Quantidade do produto: {Produtos[i,2]} |");
             }
         }
 
--- Revisao/Estoque/Program.cs
+++ Revisao/Estoque/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Estoque;
 using System;
+using System.Globalization;
 
 namespace program
 {
@@ -8,29 +9,52 @@ namespace program
         static void Main(string[] args)
         {
             int opcao;
-            Console.WriteLine("O que você deseja fazer: ");
-            Console.WriteLine("1 - Cadastrar produtos.");
-            Console.WriteLine("2 - Remover produtos.");
-            Console.WriteLine("3 - Ver Estoque.");
-            Console.WriteLine("0 - Sair.");
-            opcao = int.Parse(Console.ReadLine());
+            Produto produto = new Produto();
 
-            switch (opcao)
+            do
             {
-                case 0:
-                    Console.WriteLine("Você escolheu sair!");
-                    break;
-                case 1:
-                    Console.WriteLine("Você escolheu cadastrar um produto!");
-                    Console.Write("Nome do produto:");
+                Console.WriteLine("O que você deseja fazer: ");
+                Console.WriteLine("1 - Cadastrar produtos.");
+                Console.WriteLine("2 - Remover produtos.");
+                Console.WriteLine("3 - Ver Estoque.");
+                Console.WriteLine("0 - Sair.");
+                opcao = int.Parse(Console.ReadLine());
+                Console.WriteLine("-----------------------------------------------------------------");
+
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Você escolheu sair!");
+                        Console.ReadLine();
+                        break;
+                    case 1:
+                        Console.WriteLine("Você escolheu cadastrar um produto!");
+                        Console.Write("Nome do produto:");
                         string nome = Console.ReadLine();
-                    Console.Write("Valor do produto:");
-                        double valor = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Quantidade do produto:");
+                        Console.Write("Valor do produto:");
+                        double valor = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                        Console.WriteLine("Quantidade do produto:");
                         int quant = int.Parse(Console.ReadLine());
-                    break;
-                case 2:
-            }
+
+
+
+                        produto.CadastrarProduto(nome, valor, quant);
+
+                        Console.WriteLine("-----------------------------------------------------------------");
+                        Console.WriteLine("Produto cadastrado com sucesso!");
+
+                        Console.WriteLine("-----------------------------------------------------------------");
+
+
+                        break;
+                    case 3:
+                        Console.WriteLine("-----------------------------------------------------------------");
+                        Console.WriteLine("Você escolheu listar os produtos:");
+                        produto.ListarProdutos();
+                        Console.WriteLine("-----------------------------------------------------------------");
+                        break;
+                }
+            } while (opcao != 0);
         }
     }
 }
