5aee851b0c0fdcdd460e97f3b5a5abe805b7b5d3
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-18 21:24:32 -0300

    exemplo membro statico

---
+++ Revisao/membrosStaticos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite seu primeiro e segundo nome");
+            string[] sup = Console.ReadLine().Split(' ');
+            string nome = sup[0];
+            string sobrenome = sup[1];
+
+            Console.WriteLine("------------------------------------------------------------------------------------");
+
+            Console.WriteLine(JuntaNome(nome, sobrenome));
+
+        }
+
+        static string JuntaNome(string nome, string sobrenome)
+        {
+            return $"O nome completo é {nome} {sobrenome}.";
+        }
+    }
+}
