ae6ce085452b99a8d659328063bc0b387f84df94
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-19 23:13:14 -0300

    construindo classe de conta bancaria

--- Revisao/appBanco/ContaBancaria.cs
+++ Revisao/appBanco/ContaBancaria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,35 @@ namespace appBanco
         private string NomeTitular;
         private double SaldoInicial;
 
+        public void CriarConta(int numConta,string NomeTitular,double SaldoInicial)
+        {
 
+            if (NumConta <4)
+            {
+                this.NumConta = numConta;
+
+
+                if (SaldoInicial > 0)
+                {
+                    this.SaldoInicial = SaldoInicial;
+                }
+                else
+                {
+                    this.SaldoInicial = 0;
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("A conta deve possuir 4 digitos! Refaça o processo");
+                break;
+            }
+
+            
+            this.SaldoInicial = SaldoInicial;
+
+        }
 
     }
 }
