f597ad81f29f100c5e8b9d7ead8876dafc985ad8
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-19 22:46:46 -0300

    iniciando atividade de classe banco

---
+++ Revisao/appBanco/ContaBancaria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appBanco
+{
+    internal class ContaBancaria
+    {
+        private int NumConta;
+        private string NomeTitular;
+        private double SaldoInicial;
+
+
+
+    }
+}
