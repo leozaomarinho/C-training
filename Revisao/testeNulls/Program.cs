d5fbbc2943b7a0c3f0b6710af0640bf61d5ae0ec
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-24 21:14:10 -0300

    testando os valores nulls e metodos de validação

--- Revisao/testeNulls/Program.cs
+++ Revisao/testeNulls/Program.cs
@@ -7,14 +7,34 @@ namespace curso
         static void Main(string[] args)
         {
 
-            double? x= null;
-            double? y= 10;
+            double? x = null;
+            double? y = 10;
 
             Console.WriteLine(x.GetValueOrDefault());
             Console.WriteLine(y.GetValueOrDefault());
 
             Console.WriteLine(x.HasValue);
             Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("a variavel X possui valor null");
+            }
+
+            //gera um erro de excessão se for chamado a partir de um objeto nullable
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("a variavel Y possui o valor null");
+                    };
+
 
         }
     }
