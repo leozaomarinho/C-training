699f638e89acda660a38b34a0000331b261fa51b
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-24 21:10:15 -0300

    testando valores nulos

---
+++ Revisao/testeNulls/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double? x= null;
+            double? y= 10;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+        }
+    }
+}
