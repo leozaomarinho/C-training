b23fcd4089f78a24d51083f8eb373049b4f6dcb6
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-26 21:29:22 -0300

    treinando objetos com vetores

---
+++ Revisao/vetEmObjetos/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace vetEmObjetos
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+
+
+        public Pessoa(string nome, int idade, string email) {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome : {Nome}, Idade: {Idade}, Email: {Email}";
+        }
+
+       
+    }
+}
---
+++ Revisao/vetEmObjetos/Program.cs
@@ -0,0 +1,66 @@
+﻿using vetEmObjetos;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Quantas pessoas você deseja cadastrar? ");
+            n = int.Parse(Console.ReadLine());
+
+            Pessoa[] pessoa = new Pessoa[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o nome da pessoa: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite a idade da pessoa: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o email da pessoa: ");
+                string email = Console.ReadLine();
+
+                pessoa[i] = new Pessoa(nome, idade, email);
+
+                Console.WriteLine("----------------------------------------------------------------");
+                
+
+            }
+
+            Console.WriteLine("O que você deseja fazer? ");
+            Console.WriteLine("1 - Listar as pessoas cadastradas.");
+            Console.WriteLine("0 - Sair.");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 0:
+                    Console.WriteLine("Você escolheu sair!");
+                    break;
+                
+                case 1:
+                    Console.WriteLine("Você escolheu listar pessoas:");
+                    ListarPessoas(pessoa);
+                    
+                    break;
+
+            }
+
+
+
+
+        }
+        public static void ListarPessoas(Pessoa[] pessoas)
+        {
+
+            foreach(Pessoa pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
+        }
+
+    }
+}
