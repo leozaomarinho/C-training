20fd4e6576fef2279ab7f635bfb25e24fe93f351
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-27 21:48:42 -0300

    trabalhando com listas

---
+++ Revisao/listasTreino/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string>contato = new List<string>();
+
+            string nome;
+            int option;
+            do
+            {
+
+                Console.WriteLine("Seja bem vindo a sua lista telefonica, o que deseja fazer?");
+                Console.WriteLine("1 - Cadastrar contato.");
+                Console.WriteLine("2 - Remover contato.");
+                Console.WriteLine("3 - Visualizar contatos");
+                Console.WriteLine("0 - sair.");
+
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("-------------------------------------------------------------");
+                        Console.WriteLine("Você escolheu sair!");
+                        break;
+                    case 1:
+                        Console.WriteLine("Você escolheu cadastrar um novo contato!");
+                        Console.Write("Digite o nome do contato: ");
+                        nome = Console.ReadLine();
+                        
+
+                        contato.Add(nome);
+
+                        break;
+                }
+
+            }
+            while (option != 0);
+
+        }
+
+        
+    }
+}
+
