315288e58383037948262608ce374c837659af29
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-28 22:56:26 -0300

    matriz bidimensional

---
+++ Revisao/matrizQuadra/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,negativos =0;
+            Console.Write("Digite o valor de N da matriz: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n,n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for(int j =0; j < n; j++)
+                {
+                    mat[i,j] = int.Parse(Console.ReadLine());
+
+                    if (mat[i, j] < 0)
+                    {
+                        negativos++;
+                    }
+                }
+            }
+        }
+    }
+}
