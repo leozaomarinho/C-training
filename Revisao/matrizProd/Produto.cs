1bebc4d5d8ebd9d56953f84b8553914a14b220f4
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-30 10:40:44 -0300

    adicionar matriz produtos

---
+++ Revisao/matrizProd/Produto.cs
@@ -0,0 +1,21 @@
+﻿
+namespace matrizProd
+{
+    internal class Produto
+    {
+
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quant { get; set; }
+
+        public Produto(string nome, double preco, int quant) {
+
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quant = quant;
+        }
+
+        public void Listar (){
+}
+    }
+}
