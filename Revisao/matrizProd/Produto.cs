32cb0e519117937a3fb0ff3ea6b84a8cb010c522
Leonardo Marinho <leowalkg6@Gmail.com>
2023-07-30 10:48:38 -0300

    construindo pratica

--- Revisao/matrizProd/Produto.cs
+++ Revisao/matrizProd/Produto.cs
@@ -8,14 +8,14 @@ namespace matrizProd
         public double Preco { get; set; }
         public int Quant { get; set; }
 
-        public Produto(string nome, double preco, int quant) {
+        public Produto(string nome, double preco, int quant)
+        {
 
             this.Nome = nome;
             this.Preco = preco;
             this.Quant = quant;
         }
 
-        public void Listar (){
-}
+        
     }
 }
--- Revisao/matrizProd/Program.cs
+++ Revisao/matrizProd/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using matrizProd;
+using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Bem vindo ao cadastro de produtos ----");
+            int option;
+
+            do {
+
+                Console.WriteLine("O que você deseja fazer:");
+                Console.WriteLine("0 - Sair.");
+                Console.WriteLine("1 - Cadastrar produtos");
+                Console.WriteLine("2 - Listar  produtos");
+                option = int.Parse(Console.ReadLine());
+
+                if(option == 0)
+                {
+                    Console.WriteLine("Você escolheu sair! Até a proxima");
+                }
+
+            } while (option!=0);
+             
+        }
+    }
+}
