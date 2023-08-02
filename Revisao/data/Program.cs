5647a55ab4e40b0a628c5a34ffe894fba01dc057
Leonardo Marinho <leowalkg6@Gmail.com>
2023-08-02 10:35:50 -0300

    trabalhando com metodos de datas

--- Revisao/data/Program.cs
+++ Revisao/data/Program.cs
@@ -10,11 +10,17 @@ namespace curso
 
             Console.WriteLine($"Data atual : {d1}");
 
-            DateTime d2 = new DateTime(2023, 08, 01, 11, 30,00);
+            DateTime d2 = new DateTime(2023, 08, 05, 11, 30,00);
             Console.WriteLine($"Data com horario setado: {d2}");
 
             DateTime d3 = DateTime.Parse("05/08/1998 13:50:58");
             Console.WriteLine($"Data com horario BR {d3}");
+
+
+
+            Console.WriteLine(d2.ToLongDateString());
+            Console.WriteLine(d2.ToLongTimeString());
+            Console.WriteLine(d3.ToShortDateString());
         }
     }
 }
---
+++ Revisao/work list/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> list = new List<string>();
+
+            Console.WriteLine();
+
+        }
+    }
+}
