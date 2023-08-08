5e04206d288a194596e385d860585385d310ab37
Leonardo Marinho <leowalkg6@Gmail.com>
2023-08-07 22:00:05 -0300

    construindo atividade com classes e metodos

---
+++ Revisao/carrosValor/Program.cs
@@ -0,0 +1,26 @@
+﻿using carrosValor;
+using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Bem vindo ao cadastro de veiculos ---");
+            Console.Write("Quantos veiculos você deseja cadastrar? ");
+            int quant = int.Parse(Console.ReadLine());
+
+            Veiculos[] veic = new Veiculos[quant];
+        }
+
+        public static void listar()
+        {
+            
+
+        }
+    }
+
+
+   
+}
---
+++ Revisao/carrosValor/Veiculos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carrosValor
+{
+    internal class Veiculos
+
+    {
+        public string Modelo { get; set; }
+        public double Valor { get; set; }
+
+        public string Cor { get; set; }
+
+        public int Ano { get; set; }
+
+        public Veiculos(string model,double valor, string cor,int ano) {
+        
+        this.Modelo = model;
+            this.Valor = valor;
+            this.Cor = cor;
+            this.Ano = ano;
+        
+        }
+
+        public override string ToString()
+        {
+            return $"Modelo : {Modelo} | Valor : {Valor} | Ano : {Ano} | Cor : {Cor}";
+        }
+
+    }
+}
