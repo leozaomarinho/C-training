dcca3051033555ea37268451371d594d54100574
Leonardo Marinho <leowalkg6@Gmail.com>
2023-08-16 13:53:27 -0300

    finalizando construção das classes.

---
+++ Revisao/ExeEnumCompo/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeEnumCompo.Entities.Enums
+{
+    internal enum OrderStatus
+    {
+        PENDING_PAYMENT = 0,
+        PROCESSING = 1,
+        SHIPPED = 2,
+        DELIVERED = 3
+
+
+    }
+}
---
+++ Revisao/ExeEnumCompo/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExeEnumCompo.Entities.Enums;
+
+namespace ExeEnumCompo.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+    }
+}
