9f278f7b79aa536dc00b5879d2549f33a4d37d71
Leonardo Marinho <leowalkg6@Gmail.com>
2023-08-16 11:42:35 -0300

    criando as classes

---
+++ Revisao/ExeEnumCompo/Entities/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeEnumCompo.Entities
+{
+    internal class OrderItem
+    {
+        public int Quantity { get; set; }
+        public Double Price { get; set; }
+
+        public OrderItem() { }
+
+        public OrderItem(int quantity, Double price)
+        {
+            this.Quantity = quantity;
+            this.Price = price;
+        }
+
+        public void subTotal() { 
+        
+            
+        }
+    }
+}
---
+++ Revisao/ExeEnumCompo/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeEnumCompo.Entities
+{
+    internal class Product
+    {
+        public Double Name { get; set; }
+        public Double Price { get; set; }
+
+        public Product() { }
+        public Product(Double name, Double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
