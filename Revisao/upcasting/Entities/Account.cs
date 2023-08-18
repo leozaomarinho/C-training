abdcb9c889892f614d7afd9029fba6ad29bf3f2b
Leonardo Marinho <leowalkg6@Gmail.com>
2023-08-18 11:02:53 -0300

    atividade com herança e upcasting

---
+++ Revisao/upcasting/Entities/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upcasting.Entities
+{
+    internal class Account
+    {
+
+        public int Number { get; set; }
+        public string Holder { get; set; }
+
+        public Double Balance { get; set; }
+
+        public Account() { 
+        
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
