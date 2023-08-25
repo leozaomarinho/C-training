201a3bed375c030593b7d4928a03151cc569287d
Leonardo Marinho <leowalkg6@Gmail.com>
2023-08-25 10:43:39 -0300

    trabalhando com POO

---
+++ Revisao/PagamantoFunc/Entities/Employees.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagamantoFunc.Entities
+{
+    internal class Employees
+    {
+        public string Name { get; set; }
+        public int Hour { get; set; }
+
+        public double ValuePerHour { get; set; }
+
+
+        public void Payment()
+        {
+            double pay = ValuePerHour * Hour;
+
+            Console.WriteLine(pay.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
