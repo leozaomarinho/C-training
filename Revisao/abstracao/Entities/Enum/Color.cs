c3bd2a04ba5d252aec15c6db2d01d8fa188d9736
Leonardo Marinho <leowalkg6@Gmail.com>
2023-08-29 21:18:23 -0300

    estruturando exercicio de abstração

---
+++ Revisao/abstracao/Entities/Enum/Color.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstracao.Entities.Enum
+{
+    internal class Color
+    {
+    }
+}
---
+++ Revisao/abstracao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace abstracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
