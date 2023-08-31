1eacbc51b838702ccf33cd527d4b4b2e664e0317
Leonardo Marinho <leowalkg6@gmail.com>
2023-08-31 16:44:08 -0300

    trabalhando com abstração

--- HenrancaPoli/HenrancaPoli/Entities/Contribuintes.cs
+++ HenrancaPoli/HenrancaPoli/Entities/Contribuintes.cs
@@ -7,15 +7,23 @@ using System.Threading.Tasks;
 namespace HenrancaPoli.Entities
 {
     abstract class Contribuintes
+        //se o metodo for abstrato, a classe obrigatoriamente deve ser
     {
         public string Name { get; set; }
         public double AnualIncome { get; set; }
 
-
-
-        public abstract double Tax()
-        {
-
+        public Contribuintes() { 
+        
         }
+
+        public Contribuintes(string name, double anualIncome)
+        {
+            this.Name = name;
+            this.AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+        //metodos abstratos são criados sem argumentos, pois o abstract sinaliza que o metodo será implementando em outra classe que herde
+       //com o abstract não precisa do virtual para fazer o override do metodo
     }
 }
--- HenrancaPoli/HenrancaPoli/Entities/PessoaFisica.cs
+++ HenrancaPoli/HenrancaPoli/Entities/PessoaFisica.cs
@@ -6,9 +6,21 @@ using System.Threading.Tasks;
 
 namespace HenrancaPoli.Entities
 {
-    internal class PessoaFisica
+    internal class PessoaFisica : Contribuintes
     {
 
         public double HealthExpenditures { get; set; }
+
+        public PessoaFisica(string name,double anualIncome,double healthExp)
+        :base(name,anualIncome){ 
+        
+        }
+
+        public override double Tax()
+        {
+            ;
+        }
+
+
     }
 }
