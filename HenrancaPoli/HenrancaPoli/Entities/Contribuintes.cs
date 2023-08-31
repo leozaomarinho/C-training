c0a77479c1523c5a19d18d9f3200aa2ca3e4c7c9
Leonardo Marinho <leowalkg6@gmail.com>
2023-08-31 16:33:30 -0300

    construindo classes

---
+++ HenrancaPoli/HenrancaPoli/Entities/Contribuintes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HenrancaPoli.Entities
+{
+    abstract class Contribuintes
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+
+
+        public abstract double Tax()
+        {
+
+        }
+    }
+}
