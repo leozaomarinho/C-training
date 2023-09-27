71242b4c1ada5603100c3feef4fa12c3ffcb90ae
Leonardo Marinho <leowalkg6@gmail.com>
2023-09-26 21:36:58 -0300

    treinando

---
+++ Revisao/Treinando/Entities/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinando.Entities
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+        public string Sobrenome { get; set; }
+
+
+
+    }
+}
---
+++ Revisao/Treinando/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Treinando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
