15c435d94afe8b9e8910ee2c405a3e7ba3708399
Leonardo Marinho <leowalkg6@gmail.com>
2023-09-27 23:30:53 -0300

    resolver problema de exibição do foreach

--- Revisao/Treinando/Program.cs
+++ Revisao/Treinando/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading.Channels;
 using Treinando.Entities;
+using System.Collections.Generic;
 
 namespace Treinando
 {
@@ -9,42 +11,64 @@ namespace Treinando
         {
            List<Pessoa> pessoas  = new List<Pessoa>();
 
-            Console.WriteLine("Cadastro de pessoas:");
-
-            Console.WriteLine("Qual o seu nome e sobrenome? ex: 'Nome Sobrenome' ");
-            string[] nomeCompleto = Console.ReadLine().Split(' ');
-
-            string nome = nomeCompleto[0];
-            string sobrenome = nomeCompleto[1];
-
-            Console.WriteLine("Qual o seu CPF? ");
-            int cpf = int.Parse(Console.ReadLine());
-
-            pessoas.Add(new Pessoa(nome, sobrenome, cpf));
-
-            Console.WriteLine("Você possui filhos? s/n");
-            char filhos = char.Parse(Console.ReadLine());
-
-
-
-            if (filhos == 's')
+            try
             {
-                Console.WriteLine("Nome do filho: ");
-                string nomeFilho = Console.ReadLine();
+                Console.WriteLine("Cadastro de pessoas:");
 
-                Console.WriteLine("Nome do pai: ");
-                string nomePai = Console.ReadLine();
+                Console.WriteLine("Qual o seu nome e sobrenome? ex: 'Nome Sobrenome' ");
+                string[] nomeCompleto = Console.ReadLine().Split(' ');
 
-                Console.WriteLine("Nome da mãe: ");
-                string nomeMae = Console.ReadLine();
+                string nome = nomeCompleto[0];
+                string sobrenome = nomeCompleto[1];
 
-                pessoas.Add(new Filho(nomeFilho, sobrenome, cpf, nomePai,nomeMae));
+                Console.WriteLine("Qual o seu CPF? ");
+                int cpf = int.Parse(Console.ReadLine());
 
-                
+                pessoas.Add(new Pessoa(nome, sobrenome, cpf));
+
+                Console.WriteLine("Você possui filhos? s/n");
+                char filhos = char.Parse(Console.ReadLine());
+
+
+
+                if (filhos == 's')
+                {
+                    Console.WriteLine("Nome do filho: ");
+                    string nomeFilho = Console.ReadLine();
+
+                    Console.WriteLine("Nome do pai: ");
+                    string nomePai = Console.ReadLine();
+
+                    Console.WriteLine("Nome da mãe: ");
+                    string nomeMae = Console.ReadLine();
+
+                    pessoas.Add(new Filho(nomeFilho, sobrenome, cpf, nomePai, nomeMae));
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Cadastro concluido!");
+
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Cadastro concluido!");
+                }
+
+                Console.WriteLine("Pessoas cadastradas: ");
+
+                foreach (List<Pessoa> pessoa in pessoas)
+                {
+                    Console.WriteLine(pessoa);
+                }
+
             }
-            else
+            catch (OverflowException)
             {
-                Console.WriteLine("Cadastro concluido");
+                Console.WriteLine("CPF invalido, Refaça o processo novamente.");
+                Console.ReadLine();
+                
             }
 
 
