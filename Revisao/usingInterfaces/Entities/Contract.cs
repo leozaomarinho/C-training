476463d7c519decd93e2c33ed807e32c9ad93d6c
Leonardo Marinho <leowalkg6@gmail.com>
2023-10-03 09:47:46 -0300

    construindo serviços

--- Revisao/usingInterfaces/Entities/Contract.cs
+++ Revisao/usingInterfaces/Entities/Contract.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections.Generic;
 namespace usingInterfaces.Entities
 {
      class Contract
--- Revisao/usingInterfaces/Services/ContractService.cs
+++ Revisao/usingInterfaces/Services/ContractService.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using usingInterfaces.Services;
+using usingInterfaces.Entities;
 
-namespace usingInterfaces.Services
+namespace UsingInterfaces.Services
 {
      class ContractService
     {
@@ -14,6 +17,21 @@ namespace usingInterfaces.Services
         public ContractService(IOnlinePaymentService onlinePaymentService)
         {
             _onlinePaymentService = onlinePaymentService;
+        }
+
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months;
+
+            for (int i = 1; i<=months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
+                double fullQuota = updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota);
+
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
         }
     }
 }
