59f364c69efd92704df29cc4ca7ca87e7636ccf7
Leonardo Marinho <leowalkg6@gmail.com>
2023-10-03 09:54:42 -0300

    problema resolvido

--- Revisao/usingInterfaces/Services/PaypalService.cs
+++ Revisao/usingInterfaces/Services/PaypalService.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace usingInterfaces.Services
 {
-    internal class PaypalService
+     class PaypalService
     {
+
+        private const double FeePercentage = 0.02;
+        private const double MonthlyInterest = 0.01;
+
+        public double Interest(double amount, int months)
+        {
+            return amount * MonthlyInterest * months;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * FeePercentage;
+        }
+
     }
 }
