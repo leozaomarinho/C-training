a4ba3ed8438d8487dca1a860d4b1e92c70fe8051
Leonardo Marinho <leowalkg6@gmail.com>
2023-10-10 19:32:35 -0300

    construindo classe produto

---
+++ Revisao/Pratica10102023/Entities/Loja.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica10102023.Entities
+{
+    internal class Loja
+    {
+    }
+}
---
+++ Revisao/Pratica10102023/Entities/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica10102023.Entities
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public int Quantidade { get; set; }
+
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quantidade;
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} , Valor : {Preco} , Quantidade disponível: {Quantidade}";
+        }
+
+    }
+}
