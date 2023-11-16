5e2c386f587f001a3812c60ff86634439f28f1b0
Leonardo Marinho <leowalkg6@gmail.com>
2023-11-16 20:20:51 -0300

    prova de logica simples

---
+++ provaLogica/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int codPeca, quantPeca;
+            double valorPeca, totalPagar =0;
+
+
+
+            for(int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Digite o codigo da peça: ");
+                codPeca = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o número de peças: ");
+                quantPeca = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor da peça: ");
+                valorPeca = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                double total =(double) quantPeca * valorPeca;
+
+                totalPagar += total;
+               
+            }
+
+            Console.WriteLine($"Valor a pagar: {totalPagar.ToString("F2",CultureInfo.InvariantCulture)}");
+
+
+        }
+
+
+
+    }
+}
