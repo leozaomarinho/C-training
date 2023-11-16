c4716b795210740f1e88273c7a6b357da02cb150
Leonardo Marinho <leowalkg6@gmail.com>
2023-11-16 20:28:46 -0300

    ex 1.2 feito

---
+++ exer1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double raio;
+
+            Console.WriteLine("Digite o valor do raio de um circulo:");
+            raio = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * Math.Pow(raio,2);
+
+
+            Console.WriteLine($"A = {area.ToString("F4",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
