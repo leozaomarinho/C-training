da30ec958e977fac41b085b4c2bd0a3539d08103
Leonardo Marinho <leowalkg6@gmail.com>
2023-11-19 12:32:33 -0300

    exerc 3.1 concluido

---
+++ Exe2.2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pass = "2002";
+
+            Console.WriteLine("Digite a senha correta para ter acesso ao sistema:");
+            string passw = Console.ReadLine();
+
+            while (passw != pass)
+            {
+                if (passw != pass)
+                {
+                    Console.WriteLine("Senha invalida!");
+                    Console.WriteLine("Digite a senha correta para ter acesso ao sistema:");
+                    passw = Console.ReadLine();
+
+                }
+
+
+
+            }
+
+
+            pass.GetHashCode();
+
+            pass.GetHashCode().Equals(passw);
+
+            Console.WriteLine("Acesso Permitido!");
+
+
+
+
+        }
+    }
+}
